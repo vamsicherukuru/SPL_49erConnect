df1311fb3cf7bb46acd2838c8cbb59817b707ae6
cvams <cvamsifeb21@gmail.com>
2023-11-09 17:56:20 -0500

    Fixed Intent Integration Issues

--- app/src/main/java/com/example/a49erconnect/LoginActivity.kt
+++ app/src/main/java/com/example/a49erconnect/LoginActivity.kt
@@ -1,11 +1,25 @@
 package com.example.a49erconnect
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
+
+
+    private lateinit var btn_signup: Button;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
+        btn_signup = findViewById(R.id.btn_signup)
+        btn_signup.setOnClickListener{
+            val intent = Intent(this,SignupActivity::class.java)
+
+            startActivity(intent)
+        }
     }
 }
