8fa912c723daf76b1926d3e63516d191f6337ee4
cvams <cvamsifeb21@gmail.com>
2023-11-25 20:27:44 -0500

    Hiding Logged in user to be shown on the list

--- app/src/main/java/com/example/a49erconnect/MainActivity.kt
+++ app/src/main/java/com/example/a49erconnect/MainActivity.kt
@@ -47,7 +47,12 @@ class MainActivity : AppCompatActivity() {
                 userList.clear()
                 for (postSnapshot in snapshot.children){
                     val currentUser = postSnapshot.getValue(User::class.java)
-                    userList.add(currentUser!!)
+
+                    if(mAuth.currentUser?.uid != currentUser?.uid){
+                        userList.add(currentUser!!)
+                    }
+
+
 
                 }
 
